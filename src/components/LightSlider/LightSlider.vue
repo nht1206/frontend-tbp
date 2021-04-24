8babd82d9acef04cf9f54b69ad9ddf332ab6d1a7
nht1206 <nghuutho74@gmail.com>
2021-04-24 23:23:47 +0700

    css for prev and next button in light slider

--- src/components/LightSlider/LightSlider.vue
+++ src/components/LightSlider/LightSlider.vue
@@ -27,5 +27,69 @@ export default class extends Vue {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.lSSlideOuter {
+  .lSPager.lSGallery {
+    li {
+      margin: 0 !important;
+      border: 1px solid #ececec;
+      border-radius: 0 !important;
+      &:hover {
+        border-color: #ff9800 !important;
+      }
+    }
+    li.active {
+      border-color: #ff9800 !important;
+    }
+  }
+}
+.lSAction {
+  a {
+    opacity: 1;
+    &:after {
+      font-family: FontAwesome;
+      font-size: 25px;
+      position: absolute;
+      background: #fff;
+      width: 52px;
+      height: 52px;
+      border-radius: 100px;
+      border: 1px solid #f1f1f2;
+      transition: 0.5s;
+    }
+    &:before {
+      font-family: FontAwesome;
+      font-size: 25px;
+      position: absolute;
+      background: #fff;
+      width: 52px;
+      height: 52px;
+      border-radius: 100px;
+      border: 1px solid #f1f1f2;
+      transition: 0.5s;
+    }
+    &:hover {
+      &:after {
+        background: #ff9800;
+      }
+      &:before {
+        background: #ff9800;
+      }
+    }
+  }
+  a.lSPrev {
+    &:before {
+      content: "\f104";
+      padding: 7px 19px;
+      transform: translate(-10px);
+    }
+  }
+  a.lSNext {
+    &:after {
+      content: "\f105";
+      padding: 7px 21px;
+      transform: translate(-10px);
+    }
+  }
+}
 </style>
