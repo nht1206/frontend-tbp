9b40305a3cf49d5e246565dfd81f9aa4ae579f9e
nht1206 <nghuutho74@gmail.com>
2021-05-07 19:54:52 +0700

    add area chart component

---
+++ src/components/Admin/Chart/AreaChart.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { PolarArea, mixins } from "vue-chartjs";
+import { ChartData } from "chart.js";
+
+@Component({
+  extends: PolarArea,
+  mixins: [mixins.reactiveProp],
+})
+export default class extends Vue {
+  @Prop()
+  chartData!: ChartData;
+
+  @Prop({
+    default: function () {
+      return {};
+    },
+  })
+  options!: unknown;
+
+  public renderChart!: (chartData: ChartData, options: any) => void;
+
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  }
+}
+</script>
